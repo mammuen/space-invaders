5294167d74a550ef00b1af61d2237bb0966510ac
markuskjeldsen <kinglink224@gmail.com>
2024-01-08 10:01:44 +0100

    jeg har tilføjet timer.h og .c

---
+++ Inc/timer.h
@@ -0,0 +1,17 @@
+/*
+ * timer.h
+ *
+ *  Created on: Jan 8, 2024
+ *      Author: markus
+ */
+
+#ifndef TIMER_H_
+#define TIMER_H_
+
+
+void timerInit();
+
+
+
+
+#endif /* TIMER_H_ */
---
+++ Src/timer.c
@@ -0,0 +1,19 @@
+/*
+ * timer.c
+ *
+ *  Created on: Jan 8, 2024
+ *      Author: markus
+ */
+
+#include "timer.h"
+
+
+
+void timerInit(){
+
+
+
+
+
+}
+
