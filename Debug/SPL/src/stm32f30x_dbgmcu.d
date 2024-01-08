fa9220b94036c6da2c704f8e2eff9ef4d53b2a35
markuskjeldsen <kinglink224@gmail.com>
2024-01-08 10:30:03 +0100

    change in timer.c

--- Debug/SPL/src/stm32f30x_dbgmcu.d
+++ Debug/SPL/src/stm32f30x_dbgmcu.d
@@ -1,88 +1,59 @@
 SPL/src/stm32f30x_dbgmcu.o: ../SPL/src/stm32f30x_dbgmcu.c \
- /Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_dbgmcu.h \
- /Users/oscar/Documents/c/30010/space-invaders/Inc/stm32f30x.h \
- /Users/oscar/Documents/c/30010/space-invaders/cmsis/core_cm4.h \
- /Users/oscar/Documents/c/30010/space-invaders/cmsis/core_cmInstr.h \
- /Users/oscar/Documents/c/30010/space-invaders/cmsis/core_cmFunc.h \
- /Users/oscar/Documents/c/30010/space-invaders/cmsis/core_cm4_simd.h \
- /Users/oscar/Documents/c/30010/space-invaders/Inc/system_stm32f30x.h \
- /Users/oscar/Documents/c/30010/space-invaders/Inc/stm32f30x_conf.h \
- /Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_adc.h \
- /Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_can.h \
- /Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_crc.h \
- /Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_comp.h \
- /Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_dac.h \
- /Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_dma.h \
- /Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_exti.h \
- /Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_flash.h \
- /Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_gpio.h \
- /Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_syscfg.h \
- /Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_i2c.h \
- /Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_iwdg.h \
- /Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_opamp.h \
- /Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_pwr.h \
- /Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_rcc.h \
- /Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_rtc.h \
- /Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_spi.h \
- /Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_tim.h \
- /Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_usart.h \
- /Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_wwdg.h \
- /Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_misc.h
-
-/Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_dbgmcu.h:
-
-/Users/oscar/Documents/c/30010/space-invaders/Inc/stm32f30x.h:
-
-/Users/oscar/Documents/c/30010/space-invaders/cmsis/core_cm4.h:
-
-/Users/oscar/Documents/c/30010/space-invaders/cmsis/core_cmInstr.h:
-
-/Users/oscar/Documents/c/30010/space-invaders/cmsis/core_cmFunc.h:
-
-/Users/oscar/Documents/c/30010/space-invaders/cmsis/core_cm4_simd.h:
-
-/Users/oscar/Documents/c/30010/space-invaders/Inc/system_stm32f30x.h:
-
-/Users/oscar/Documents/c/30010/space-invaders/Inc/stm32f30x_conf.h:
-
-/Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_adc.h:
-
-/Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_can.h:
-
-/Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_crc.h:
-
-/Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_comp.h:
-
-/Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_dac.h:
-
-/Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_dma.h:
-
-/Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_exti.h:
-
-/Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_flash.h:
-
-/Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_gpio.h:
-
-/Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_syscfg.h:
-
-/Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_i2c.h:
-
-/Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_iwdg.h:
-
-/Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_opamp.h:
-
-/Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_pwr.h:
-
-/Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_rcc.h:
-
-/Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_rtc.h:
-
-/Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_spi.h:
-
-/Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_tim.h:
-
-/Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_usart.h:
-
-/Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_wwdg.h:
-
-/Users/oscar/Documents/c/30010/space-invaders/SPL/inc/stm32f30x_misc.h:
+ /home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_dbgmcu.h \
+ /home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/Inc/stm32f30x.h \
+ /home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/cmsis/core_cm4.h \
+ /home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/cmsis/core_cmInstr.h \
+ /home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/cmsis/core_cmFunc.h \
+ /home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/cmsis/core_cm4_simd.h \
+ /home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/Inc/system_stm32f30x.h \
+ /home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/Inc/stm32f30x_conf.h \
+ /home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_adc.h \
+ /home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_can.h \
+ /home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_crc.h \
+ /home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_comp.h \
+ /home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_dac.h \
+ /home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_dma.h \
+ /home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_exti.h \
+ /home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_flash.h \
+ /home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_gpio.h \
+ /home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_syscfg.h \
+ /home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_i2c.h \
+ /home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_iwdg.h \
+ /home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_opamp.h \
+ /home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_pwr.h \
+ /home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_rcc.h \
+ /home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_rtc.h \
+ /home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_spi.h \
+ /home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_tim.h \
+ /home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_usart.h \
+ /home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_wwdg.h \
+ /home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_misc.h
+/home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_dbgmcu.h:
+/home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/Inc/stm32f30x.h:
+/home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/cmsis/core_cm4.h:
+/home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/cmsis/core_cmInstr.h:
+/home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/cmsis/core_cmFunc.h:
+/home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/cmsis/core_cm4_simd.h:
+/home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/Inc/system_stm32f30x.h:
+/home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/Inc/stm32f30x_conf.h:
+/home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_adc.h:
+/home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_can.h:
+/home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_crc.h:
+/home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_comp.h:
+/home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_dac.h:
+/home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_dma.h:
+/home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_exti.h:
+/home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_flash.h:
+/home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_gpio.h:
+/home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_syscfg.h:
+/home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_i2c.h:
+/home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_iwdg.h:
+/home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_opamp.h:
+/home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_pwr.h:
+/home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_rcc.h:
+/home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_rtc.h:
+/home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_spi.h:
+/home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_tim.h:
+/home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_usart.h:
+/home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_wwdg.h:
+/home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/SPL/inc/stm32f30x_misc.h:
