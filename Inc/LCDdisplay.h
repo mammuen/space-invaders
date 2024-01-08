fa9a50fde28484f0d5f876c221e90952ea594811
markuskjeldsen <kinglink224@gmail.com>
2024-01-08 11:41:23 +0100

    lcd display added

---
+++ Inc/LCDdisplay.h
@@ -0,0 +1,16 @@
+/*
+ * LCDdisplay.h
+ *
+ *  Created on: Jan 8, 2024
+ *      Author: markus
+ */
+
+#ifndef LCDDISPLAY_H_
+#define LCDDISPLAY_H_
+
+
+void displayString(uint8_t* array, int x, int y);
+extern const char character_data[95][5];
+
+
+#endif /* LCDDISPLAY_H_ */
