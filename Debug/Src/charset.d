6d53bcc9470c4fe80b72516334256107a470946e
DoubelKiller <doubel.killer@gmail.com>
2024-01-08 10:46:23 +0100

    Test

--- Debug/Src/charset.d
+++
@@ -1,3 +0,0 @@
-Src/charset.o: ../Src/charset.c \
- /home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/Inc/charset.h
-/home/markus/STM32CubeIDE/workspace_1.14.0/project_30010/space-invaders/Inc/charset.h:
