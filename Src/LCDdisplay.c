a66a60c6bcf36fb0f73c3c4fa6e748bbcfa328fe
markuskjeldsen <kinglink224@gmail.com>
2024-01-08 12:35:38 +0100

    changed lcd

--- Src/LCDdisplay.c
+++ Src/LCDdisplay.c
@@ -1,4 +1,40 @@
-#include "charset.h"
+/*
+ * LCDdisplay.c
+ *
+ *  Created on: Jan 8, 2024
+ *      Author: markus
+ */
+
+void displayString(uint8_t* array, int x, int y){
+	int length = strlen(array);
+	int arr[512];
+
+	for(int p = 0; p < length; p++){
+		arr[p] = ((uint8_t) array[p] - 32);
+	}
+
+	x = x%(128-(length+1)*4);
+	y = y%4;
+
+
+
+	uint8_t format[512];
+	memset(format,0,512);
+
+	for(int i = 0; i < length ; i++){
+
+		format[ i*5  + 0 + x + y*128] = character_data[ arr[i] ][0];
+		format[(i*5) + 1 + x + y*128] = character_data[ arr[i] ][1];
+		format[(i*5) + 2 + x + y*128] = character_data[ arr[i] ][2];
+		format[(i*5) + 3 + x + y*128] = character_data[ arr[i] ][3];
+		format[(i*5) + 4 + x + y*128] = character_data[ arr[i] ][4];
+
+	}
+	lcd_push_buffer(format);
+
+}
+
+
 
 const char character_data[95][5] = {
   {0x00, 0x00, 0x00, 0x00, 0x00},
@@ -96,3 +132,5 @@ const char character_data[95][5] = {
   {0x00, 0x00, 0x7F, 0x00, 0x00},
   {0x00, 0x41, 0x36, 0x08, 0x00},
   {0x08, 0x04, 0x08, 0x10, 0x08}};
+
+
