c057446f46f40b93e579d9de5e3fa33b64c9098e
markuskjeldsen <kinglink224@gmail.com>
2024-01-08 10:15:00 +0100

    change in timer.c

--- Src/timer.c
+++ Src/timer.c
@@ -12,6 +12,21 @@
 void timerInit(){
 
 	RCC ->APBWENR |= RCC_APB2Periph_TIM15;
+	TIM15->CR1 = TIM15->CR1 & ~(0x001); 	// turn off timer
+
+	TIM15->ARR = 999; 	// Set reload value
+
+	TIM15->PSC = 639; 	// Set prescale value
+	TIM15->DIER |= 0x0001; // enable timer 15's interrutps
+	// calls interrupt ever 1 ms
+
+
+
+	TIM15->CR1 = TIM15->CR1 | (0x001); 	// turn on timer
+
+	NVIC_SetPriority(TIM1_BRK_TIM15_IRQn, 5);
+	NVIC_EnableIRQ(TIM1_BRK_TIM15_IRQn);
+
 
 
 
