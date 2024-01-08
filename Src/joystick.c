daab30ddeda733d6a49ae95551ff907c12774dbf
DoubelKiller <doubel.killer@gmail.com>
2024-01-08 12:29:08 +0100

    Update joystick.c

--- Src/joystick.c
+++ Src/joystick.c
@@ -1,18 +1,6 @@
 #include "joystick.h"
 #include "stm32f30x.h"
 
-char readJoystick(){
-		char up = readgpioA4() >> 1;
-		char down = readgpioB0() << 1;
-		char left = readgpioC1() << 1;
-		char right = readgpioC0();
-		char center = readgpioB5() >> 1;
-
-		char output = up | down | left | right | center;
-		return output;
-}
-
-
 int ledsetup(){
 
 	// set PA9 to output
@@ -48,6 +36,8 @@ int ledsetup(){
 	GPIOB->MODER &= ~(0x00000003 << (pin * 2)); // Clear mode register
 	GPIOB->MODER |= (0x00000001 << (pin * 2)); // Set mode register (0x00 –Input, 0x01 - Output, 0x02 - Alternate Function, 0x03 - Analog in/out)
 
+	return 0;
+
 }
 
 int setup(){
@@ -55,7 +45,6 @@ int setup(){
 	RCC->AHBENR |= RCC_AHBPeriph_GPIOA; // Enable clock for GPIO Port A
 	RCC->AHBENR |= RCC_AHBPeriph_GPIOB; // Enable clock for GPIO Port B
 	RCC->AHBENR |= RCC_AHBPeriph_GPIOC; // Enable clock for GPIO Port C
-
 
 	// Set pin PA4 to input
 	GPIOA->MODER &= ~(0x00000003 << (4 * 2)); // Clear mode register
@@ -68,7 +57,6 @@ int setup(){
 	GPIOB->PUPDR &= ~(0x00000003 << (0 * 2)); // Clear push/pull register
 	GPIOB->PUPDR |= (0x00000002 << (0 * 2)); // Set push/pull register (0x00 - No pull, 0x01 - Pull-up, 0x02 - Pull-down)
 
-
 	// Set pin PC1 and PC0 to input
 	GPIOC->MODER &= ~(0x0000000f << (0 * 2)); // Clear mode register for the first 4 bit
 	GPIOC->MODER |= (0x00000000 << (0 * 2)); // Set mode register
@@ -77,13 +65,11 @@ int setup(){
 	GPIOC->PUPDR |= (0x0000000c << (0 * 2)); // Set push/pull register
 	//(0x00 - No pull, 0x01 - Pull-up, 0x02 - Pull-down)
 
-
 	// Set pin PB5 to input
 	GPIOB->MODER &= ~(0x00000003 << (5 * 2)); // Clear mode register
 	GPIOB->MODER |= (0x00000000 << (5 * 2)); // Set mode register (0x00 –Input, 0x01 - Output, 0x02	// Alternate Function, 0x03 - Analog in/out)
 	GPIOB->PUPDR &= ~(0x00000003 << (5 * 2)); // Clear push/pull register
 	GPIOB->PUPDR |= (0x00000002 << (5 * 2)); // Set push/pull register (0x00 - No pull, 0x01 - Pull-up, 0x02 - Pull-down)
-
 
 /*
 	// Set pin PA1 to output
@@ -190,4 +176,15 @@ int readgpioB5(){
 	char val = GPIOB->IDR & (0x0001 << 5); //Read from pin PB0
 
 	return val;
+}
+
+char readJoystick(){
+		char up = readgpioA4() >> 1;
+		char down = readgpioB0() << 1;
+		char left = readgpioC1() << 1;
+		char right = readgpioC0();
+		char center = readgpioB5() >> 1;
+
+		char output = up | down | left | right | center;
+		return output;
 }
