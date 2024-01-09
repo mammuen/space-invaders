6e98ecad9e1624365fdb3c44e2ab6c541beda701
markuskjeldsen <kinglink224@gmail.com>
2024-01-09 13:19:53 +0100

    game.c/h update

---
+++ Inc/collision.c
@@ -0,0 +1,41 @@
+/*
+ * collision.c
+ *
+ *  Created on: Jan 9, 2024
+ *      Author: markus
+ */
+
+#include "collision.h"
+
+
+
+int checkcollision(uint8_t* collision, player p, asteriod a, enemy e)
+{
+
+	return 0;
+}
+
+
+uint8_t* initCollision(){
+	uint8_t* line = malloc(180*sizeof(uint8_t));
+	free(line);
+	return line;
+}
+
+void cleararray(uint8_t* collision){
+	memset(collision, 0, 180);
+}
+
+
+
+
+
+
+
+
+
+
+
+//= {
+//{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+//}
