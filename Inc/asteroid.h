7aae4affbef87ab6f5ddf480472ff0eee1395d1f
DoubelKiller <doubel.killer@gmail.com>
2024-01-10 13:45:40 +0100

    add asteroid.c / .h

---
+++ Inc/asteroid.h
@@ -0,0 +1,13 @@
+/*
+ * asteroid.h
+ *
+ *  Created on: 10 янв. 2024 г.
+ *      Author: doube
+ */
+
+#ifndef ASTEROID_H_
+#define ASTEROID_H_
+
+
+
+#endif /* ASTEROID_H_ */
---
+++ Src/asteroid.c
@@ -0,0 +1,8 @@
+/*
+ * asteroid.c
+ *
+ *  Created on: 10 янв. 2024 г.
+ *      Author: doube
+ */
+
+
