a313ffdad79e576aa200c9a0867b3264199c5c09
markuskjeldsen <kinglink224@gmail.com>
2024-01-12 11:25:26 +0100

    Main filen er blevet pushet

--- Inc/collision.c
+++ Inc/collision.c
@@ -11,19 +11,22 @@
 
 int BulletEnemycollision(Enemy* E, Bullet* B)
 {
-	int diff = 4;
+	int diff = 2;
 
 	for(int i = 0; i < 6; i++){
 		for(int j = 0; j < 6; j++){
-			if( abs(B[i].x - E[i].x+1) < diff ){
-				if( abs(B[i].y - E[i].y+2) < diff ){
+			if(B[i].health != 0){
+
+ 			if( abs(B[i].x - E[j].x+1) < diff ){
+				if( abs(B[i].y - E[j].y - 1) < diff ){
 
 					B[i].health = 0;
-					E[i].health = 0;
+					E[j].health = 0;
 
 
 			}
 		}
+	}
 	}
 }
 	return 0;
